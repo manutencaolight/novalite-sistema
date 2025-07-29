15861631fbd2401c19b40821718d7cbafe339e0d
leonardo <manutemcao.light@gmail.com>
2025-07-29 14:39:01 -0300

    Adiciona e configura o tema Jazzmin

--- novalite_web/novalite_web/settings.py
+++ novalite_web/novalite_web/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS.append('localhost')
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
