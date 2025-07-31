775782427845176d99d5d99905cd127f5f74840a
leonardo <manutemcao.light@gmail.com>
2025-07-31 18:05:33 -0300

    Corrige import da permissão IsAuthenticated

--- novalite_web/core/views.py
+++ novalite_web/core/views.py
@@ -10,6 +10,7 @@ from django.utils import timezone
 from rest_framework import viewsets, status, filters, permissions
 from django.db.models import Sum
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
