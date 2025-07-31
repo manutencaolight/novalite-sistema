cfc666d1fe06033a7fe4c54cc402adea19466932
leonardo <manutemcao.light@gmail.com>
2025-07-31 17:59:46 -0300

    Corrige import da permissão AllowAny

--- novalite_web/core/views.py
+++ novalite_web/core/views.py
@@ -10,7 +10,6 @@ from django.utils import timezone
 from rest_framework import viewsets, status, filters, permissions
 from django.db.models import Sum
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
