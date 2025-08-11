570ff6b32636aff1218207e4ec8e9401f795b97b
leonardo <manutemcao.light@gmail.com>
2025-08-11 10:19:14 -0300

    adiciona import e export no backend

--- novalite_web/core/admin.py
+++ novalite_web/core/admin.py
@@ -1,19 +1,22 @@
-# Em: core/admin.py (Versão Reordenada)
+# Em: core/admin.py (Versão Final Consolidada)
 
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 from django.db import models
-from import_export import resources
+from import_export import resources, fields, widgets
 from import_export.admin import ImportExportModelAdmin
 from django.contrib.auth.admin import UserAdmin
 
 from .models import (
     Cliente, Equipamento, Funcionario, Veiculo, Evento,
-    MaterialEvento, FotoPreEvento, Usuario, ItemRetornado, Consumivel, ConsumivelEvento, RegistroManutencao
+    MaterialEvento, FotoPreEvento, Usuario, ItemRetornado, Consumivel, ConsumivelEvento,
+    RegistroManutencao
 )
 
-# --- Resources e Admins Básicos ---
+
+# --- Seção 1: Resources para Importação/Exportação ---
+
 class EquipamentoResource(resources.ModelResource):
     class Meta:
         model = Equipamento
@@ -27,31 +30,42 @@ class EquipamentoResource(resources.ModelResource):
             if Equipamento.objects.filter(modelo__iexact=modelo).exists():
                 kwargs['skip_row'] = True
 
-@admin.register(Cliente)
-class ClienteAdmin(admin.ModelAdmin):
-    search_fields = ('empresa', 'representante')
-    list_display = ('empresa', 'representante', 'telefone', 'email')
+class ClienteResource(resources.ModelResource):
+    class Meta:
+        model = Cliente
+        fields = ('id', 'empresa', 'representante', 'endereco', 'email', 'telefone', 'telefone_representante')
 
-@admin.register(Equipamento)
-class EquipamentoAdmin(ImportExportModelAdmin):
-    resource_classes = [EquipamentoResource]
-    search_fields = ('modelo', 'fabricante')
-    list_display = ('modelo', 'fabricante', 'categoria', 'quantidade_estoque')
-    list_filter = ('categoria',)
+class FuncionarioResource(resources.ModelResource):
+    class Meta:
+        model = Funcionario
+        fields = ('id', 'nome', 'funcao', 'tipo', 'email', 'contato')
 
-@admin.register(Funcionario)
-class FuncionarioAdmin(admin.ModelAdmin):
-    search_fields = ('nome', 'funcao')
-    list_display = ('nome', 'funcao', 'tipo', 'contato', 'email')
-    list_filter = ('tipo',)
+class VeiculoResource(resources.ModelResource):
+    class Meta:
+        model = Veiculo
+        fields = ('id', 'nome', 'placa', 'tipo', 'status')
 
-@admin.register(Veiculo)
-class VeiculoAdmin(admin.ModelAdmin):
-    search_fields = ('nome', 'placa')
-    list_display = ('nome', 'placa', 'tipo', 'status')
-    list_filter = ('status',)    
+class EventoResource(resources.ModelResource):
+    cliente = fields.Field(
+        column_name='cliente',
+        attribute='cliente',
+        widget=widgets.ForeignKeyWidget(Cliente, 'empresa'))
+    equipe = fields.Field(
+        column_name='equipe',
+        attribute='equipe',
+        widget=widgets.ManyToManyWidget(Funcionario, field='nome', separator=', '))
+    veiculos = fields.Field(
+        column_name='veiculos',
+        attribute='veiculos',
+        widget=widgets.ManyToManyWidget(Veiculo, field='nome', separator=', '))
 
-# --- Classes 'Inline' (Devem ser definidas ANTES de serem usadas) ---
+    class Meta:
+        model = Evento
+        fields = ('id', 'nome', 'status', 'tipo_evento', 'local', 'cliente', 'data_evento', 'data_termino', 'equipe', 'veiculos', 'criado_por__username')
+        export_order = fields
+
+
+# --- Seção 2: Classes 'Inline' (Definidas antes de serem usadas) ---
 
 class MaterialEventoInline(admin.TabularInline):
     model = MaterialEvento
@@ -66,45 +80,65 @@ class FotoPreEventoInline(admin.TabularInline):
     model = FotoPreEvento
     extra = 1
 
-# --- CORREÇÃO: CLASSE MOVIDA PARA CIMA ---
 class ConsumivelEventoInline(admin.TabularInline):
     model = ConsumivelEvento
     extra = 1
-    autocomplete_fields = ('consumivel',)    
+    autocomplete_fields = ('consumivel',)
 
-# --- Admin Principal do Evento (Usa as classes 'Inline' acima) ---
+
+# --- Seção 3: Model Admins ---
+
+@admin.register(Cliente)
+class ClienteAdmin(ImportExportModelAdmin):
+    resource_classes = [ClienteResource]
+    search_fields = ('empresa', 'representante')
+    list_display = ('empresa', 'representante', 'telefone', 'email')
+
+@admin.register(Equipamento)
+class EquipamentoAdmin(ImportExportModelAdmin):
+    resource_classes = [EquipamentoResource]
+    search_fields = ('modelo', 'fabricante')
+    list_display = ('modelo', 'fabricante', 'categoria', 'quantidade_estoque')
+    list_filter = ('categoria',)
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(ImportExportModelAdmin):
+    resource_classes = [FuncionarioResource]
+    search_fields = ('nome', 'funcao')
+    list_display = ('nome', 'funcao', 'tipo', 'contato', 'email')
+    list_filter = ('tipo',)
+
+@admin.register(Veiculo)
+class VeiculoAdmin(ImportExportModelAdmin):
+    resource_classes = [VeiculoResource]
+    search_fields = ('nome', 'placa')
+    list_display = ('nome', 'placa', 'tipo', 'status')
+    list_filter = ('status',)
 
 @admin.register(Evento)
-class EventoAdmin(admin.ModelAdmin):
+class EventoAdmin(ImportExportModelAdmin):
+    resource_classes = [EventoResource]
     fieldsets = (
         ('Status e Tipo da Operação', {'fields': ('status', 'tipo_evento')}),
         ('Informações Principais', {'fields': ('nome', 'local', 'cliente', 'responsavel_local_nome', 'responsavel_local_contato')}),
         ('Datas', {'fields': ('data_montagem', 'data_evento', 'data_termino')}),
         ('Recursos Designados', {'fields': ('equipe', 'veiculos')}),
     )
-    # Agora o Python já conhece todas estas classes 'Inline'
     inlines = [MaterialEventoInline, FotoPreEventoInline, ConsumivelEventoInline]
     list_display = ('nome', 'tipo_evento', 'cliente', 'data_evento', 'status', 'acao_do_evento')
     list_filter = ('status', 'tipo_evento', 'data_evento')
     search_fields = ('nome', 'cliente__empresa', 'local')
     autocomplete_fields = ('cliente', 'equipe', 'veiculos')
-    class Media:
-        css = {'all': ('core/css/admin_extra.css',)}
+
     def acao_do_evento(self, obj):
         url = reverse('evento_report_pdf', args=[obj.pk])
         return format_html('<a href="{}" class="button imprimir-button-lista" target="_blank">Imprimir</a>', url)
     acao_do_evento.short_description = 'Relatório Completo'
 
-# --- Outros Admins ---
-
 @admin.register(Usuario)
 class UsuarioAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + (
-        ('Nível de Acesso Customizado', {'fields': ('role',)}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Nível de Acesso Customizado', {'fields': ('role',)}),
-    )
+    fieldsets = UserAdmin.fieldsets + (('Nível de Acesso Customizado', {'fields': ('role',)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (('Nível de Acesso Customizado', {'fields': ('role',)}),)
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'role')
     list_filter = UserAdmin.list_filter + ('role',)
 
@@ -119,4 +153,4 @@ class RegistroManutencaoAdmin(admin.ModelAdmin):
     list_display = ('os_number', 'equipamento', 'status', 'data_entrada', 'data_saida')
     list_filter = ('status', 'data_entrada')
     search_fields = ('os_number', 'equipamento__modelo', 'descricao_problema')
-    readonly_fields = ('os_number', 'data_entrada', 'data_saida')    
+    readonly_fields = ('os_number', 'data_entrada', 'data_saida')
